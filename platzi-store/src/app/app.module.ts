eff90a9d7747c738ecd3c7fcab4c211943029f5f
jdbugames <jdbu.games@gmail.com>
2020-06-19 16:59:13 -0500

    Day06
    Se crea un componente para el home y asi optimizar.

--- platzi-store/src/app/app.module.ts
+++ platzi-store/src/app/app.module.ts
@@ -7,17 +7,16 @@ import { ProductComponent } from './components/product.component';
 import { CartComponent } from './cart/cart.component';
 import { ExponentialPipe } from './exponential.pipe';
 import { HighlightDirective } from './highlight.directive';
-import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
 import { ContactComponent } from './contact/contact.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DemoComponent } from './demo/demo.component';
 import { FooterComponent } from './footer/footer.component';
-import { BannerComponent } from './banner/banner.component';
 import { HeaderComponent } from './header/header.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { LayoutComponent } from './layout/layout.component';
+
 
 
 @NgModule({
@@ -27,12 +26,10 @@ import { LayoutComponent } from './layout/layout.component';
     CartComponent,
     ExponentialPipe,
     HighlightDirective,
-    HomeComponent,
     ProductsComponent,
     ContactComponent,
     DemoComponent,
     FooterComponent,
-    BannerComponent,
     HeaderComponent,
     PageNotFoundComponent,
     ProductDetailComponent,
